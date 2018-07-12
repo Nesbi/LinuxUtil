fff2515227fb25e2f1b175e570943affcbca6e3d
Nico Balbach <nico.balbach@gmail.com>
2018-07-12 21:20:28 +0200

    Added additional notifications

--- i3/install_solus.sh
+++ i3/install_solus.sh
@@ -11,21 +11,34 @@ I3GNOME=/opt/i3-gnome/
 # Install i3
 echo Installing i3..
 sudo eopkg install -y i3
-echo finished
 
 # Install i3-GNOME
 echo Installing i3-GNOME..
 sudo git clone https://github.com/jcstr/i3-gnome.git $I3GNOME
 cd $I3GNOME
 make install
-echo finished
 
+# Installing dependencies (rofi, compton, font-awesome-ttf)
+echo Installing dependencies \(rofi, compton, font-awesome-ttf\)
 sudo eopkg install -y rofi compton font-awesome-ttf
+
 if [ -f /home/$SUDO_USER/.config/i3/config]; then
 	echo Found existing i3 config
 	echo -\> Backup i3 config to /home/$SUDO_USER/backup/i3/config
 	sudo -u $SUDO_USER mkdir -p /home/$SUDO_USER/backup/i3
+	sudo -u $SUDO_USER cp /home/$SUDO_USER/.config/i3/config /home/$SUDO_USER/backup/i3/config
 fi
+echo Installing i3 config
+sudo -u $SUDO_USER cp config /home/$SUDO_USER/.config/i3/config
+
+if [ -f /home/$SUDO_USER/.i3status.conf]; then
+	echo Found existing i3 status config
+	echo -\> Backup i3 config to /home/$SUDO_USER/backup/i3/i3status.conf
+	sudo -u $SUDO_USER mkdir -p /home/$SUDO_USER/backup/i3
+	sudo -u $SUDO_USER cp /home/$SUDO_USER/.i3status.conf /home/$SUDO_USER/backup/i3/i3status.conf
+fi
+echo Installing i3status.conf
+sudo -u $SUDO_USER cp .i3status.conf /home/$SUDO_USER/.i3status.conf
 
 echo Installing darkdawn.rasi theme
 sudo -u $SUDO_USER cp rofi/darkdawn.rasi /usr/share/rofi/themes/
