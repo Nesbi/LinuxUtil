fc3744eac2f82de3724ffddc76c895e85e27f3bd
Nico Balbach <Nico.Balbach@gmail.com>
2019-01-18 11:56:44 +0100

    Set focus of dmux bacḱ to main pane after creation

--- tmux/dmux.sh
+++ tmux/dmux.sh
@@ -4,4 +4,5 @@ tmux split-window -v -p 30
 tmux split-window -h -p 40 'htop'
 tmux split-window -v -p 70 'watch -n 60 -t "df -h --type=ext4"'
 tmux split-window -v -p 50 'watch -n 60 -t "who -u  | grep -v tmux"'
+tmux select-pane -t 0
 tmux -2 attach-session -d
