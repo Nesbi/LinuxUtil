9f131f78a26bd017d5be4fede3cda7c29e8a8f69
Nico Balbach <Nico.Balbach@gmail.com>
2020-03-29 16:15:04 +0200

    Update i3 install scripts

--- i3/install_solus.sh
+++ i3/install_solus.sh
@@ -22,6 +22,8 @@ make install
 echo Installing dependencies \(rofi, compton, font-awesome-4\)
 sudo eopkg install -y rofi compton font-awesome-4
 
+
+
 if [ -f /home/$SUDO_USER/.config/i3/config ]; then
 	echo Found existing i3 config
 	echo -\> Backup i3 config to /home/$SUDO_USER/backup/i3/config
@@ -29,6 +31,7 @@ if [ -f /home/$SUDO_USER/.config/i3/config ]; then
 	sudo -u $SUDO_USER cp /home/$SUDO_USER/.config/i3/config /home/$SUDO_USER/backup/i3/config
 fi
 echo Installing i3 config
+sudo -u $SUDO_USER mkdir -p /home/$SUDO_USER/.config/i3
 sudo -u $SUDO_USER cp config /home/$SUDO_USER/.config/i3/config
 
 if [ -f /home/$SUDO_USER/.i3status.conf ]; then
@@ -42,8 +45,11 @@ sudo -u $SUDO_USER cp i3status.conf /home/$SUDO_USER/.config/i3/i3status.conf
 sudo -u $SUDO_USER cp lock.png /home/$SUDO_USER/.config/i3/
 
 echo Installing darkdawn.rasi theme
-sudo -u $SUDO_USER cp rofi/darkdawn.rasi /usr/share/rofi/themes/
+sudo mkdir /usr/share/rofi/themes/
+sudo cp rofi/darkdawn.rasi /usr/share/rofi/themes/
 echo Setting darkdawn.rasi as default rofi theme
+
+sudo -u $SUDO_USER mkdir -p /home/$SUDO_USER/.config/rofi
 sudo echo rofi.theme: /usr/share/rofi/themes/darkdawn.rasi  >> /home/$SUDO_USER/.config/rofi/config
 
 echo Installation is finished
